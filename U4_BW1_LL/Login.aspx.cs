d428f5b45ae67d3ca065c60ed5dc0327f5d69566
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2024-02-20 13:22:49 +0100

    fix login

--- U4_BW1_LL/Login.aspx.cs
+++ U4_BW1_LL/Login.aspx.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Configuration;
 using System.Data.SqlClient;
-using System.Threading;
 using System.Web;
 
 namespace U4_BW1_LL
@@ -56,12 +55,11 @@ namespace U4_BW1_LL
                             if (userFounded)
                             {
                                 LoginSuccessful(accediNome.Text, accediPassword.Text);
-                                Thread.Sleep(2000);
                                 MakeFieldsEmpty();
 
                             }
 
-                            Response.Redirect("Default.aspx");
+                            // Response.Redirect("Default.aspx");
                         }
                         else if (!reader.HasRows)
                         {
@@ -70,8 +68,6 @@ namespace U4_BW1_LL
 
                         }
                     }
-
-
                 }
                 catch (Exception ex)
                 {
@@ -101,9 +97,7 @@ namespace U4_BW1_LL
 
         protected void LoginSuccessful(string nome, string cognome)
         {
-            notFound_OrSuccess.Visible = true;
-            notFound_OrSuccess.InnerText = $"Bentornato Mr.{nome} {cognome}...";
-            ClientScript.RegisterStartupScript(this.GetType(), "hideAlert", "setTimeout(function() { document.getElementById('alertNot_foundOrSuccess').style.display = 'none'; }, 3000);", true);
+            Response.Redirect("Default.aspx");
         }
 
         protected void UtenteNonTrovato(string nome)
@@ -145,226 +139,6 @@ namespace U4_BW1_LL
                 alert.Visible = true;
             }
         }
-
-
-        //protected void Login_Click(object sender, EventArgs e)
-        //{
-        //    Response.Write("Login cliccato");
-        //    bool Admin = CheckAdmin(accediNome.Text, accediCognome.Text);
-
-
-        //    if (Admin)
-        //    {
-        //        //Creo un Cookie di Login per verificare l'accesso dell'utente
-        //        HttpCookie loginCookie = new HttpCookie("LOGIN_COOKIE");
-        //        loginCookie.Values["userName"] = accediNome.Text;
-        //        loginCookie.Values["password"] = accediCognome.Text;
-        //        loginCookie.Values["admin"] = (Admin ? "true" : "false");
-
-        //        loginCookie.Expires = DateTime.Now.AddDays(10);
-        //        loginCookie.Secure = true;
-        //        Response.Cookies.Add(loginCookie);
-
-
-        //        // Se non trova un cookie relativo al carrelo lo crea
-        //        /*
-        //            if (Request.Cookies["CART_COOKIE"] == null)
-        //            {
-        //                //Se non esiste creo il cookie della cart al login
-
-        //                // creo un arraylist e il cookie
-        //                ArrayList cart = new ArrayList();
-        //                HttpCookie cartCookie = new HttpCookie("CART_COOKIE");
-
-        //                //creo un converter in json
-        //                JavaScriptSerializer serializer = new JavaScriptSerializer();
-        //                string cartJson = serializer.Serialize(cart);
-
-        //                //aggiungo il json al cookie
-        //                cartCookie.Values["cart"] = cartJson;
-        //                Response.Cookies.Add(cartCookie);
-
-        //            }
-        //        */
-
-        //        //passo alla prima pagina
-        //        Response.Redirect("Default.aspx");
-        //    }
-
-        //}
-
-
-
-
-
-        //protected bool CheckAdmin(string user, string password)
-        //{
-        //    bool admin = false;
-
-        //    string connectionString = ConfigurationManager.ConnectionStrings["connectionStringDb"].ToString();
-
-        //    try
-        //    {
-        //        using (SqlConnection conn = new SqlConnection(connectionString))
-        //        {
-        //            conn.Open();
-        //            SqlCommand cmd = new SqlCommand();
-        //            cmd.Connection = conn;
-        //            cmd.CommandText = "SELECT * FROM Utenti WHERE Username = @username AND Password = @password";
-        //            cmd.Parameters.AddWithValue("@username", user);
-        //            cmd.Parameters.AddWithValue("@password", password);
-
-        //            using (SqlDataReader reader = cmd.ExecuteReader())
-        //            {
-        //                if (reader.Read())
-        //                {
-
-        //                    int adminValue = Convert.ToInt16(reader["Admin"]);
-        //                    admin = (adminValue == 1 ? true : false);
-        //                    Response.Write("Utente Admin Trovato");
-        //                    Response.Write(admin);
-
-        //                }
-        //            }
-        //        }
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        // Log dell'errore e messaggio all'utente
-        //        Response.Write("Errore durante l'autenticazione.");
-        //        Response.Write(ex.Message);
-
-        //    }
-
-        //    finally
-        //    {
-
-        //        connectionString.Clone();
-        //    }
-
-
-
-        //    return admin;
-        //}
-
-
-
-        //protected void RegistrationButton_Click(object sender, EventArgs e)
-        //{
-
-        //    string username = TextBoxUsername.Text;
-        //    string password = TextBoxPass.Text;
-        //    string passwordControl = TextBoxPassControl.Text;
-
-
-        //    if (CheckUsername(username))
-        //    {
-
-        //        if (password == passwordControl)
-        //        {
-
-
-        //            string connectionString = ConfigurationManager.ConnectionStrings["connectionStringDB"].ConnectionString.ToString();
-        //            SqlConnection conn = new SqlConnection(connectionString);
-
-        //            try
-        //            {
-
-        //                conn.Open();
-        //                SqlCommand cmd = new SqlCommand();
-        //                cmd.Connection = conn;
-        //                cmd.CommandText = "INSERT INTO Utenti (Username, Password, Admin) VALUES ( @username , @password, 0)";
-        //                cmd.Parameters.AddWithValue("@username", username);
-        //                cmd.Parameters.AddWithValue("@password", password);
-
-        //                cmd.ExecuteNonQuery();
-
-        //                //RegistrationModalBody.InnerText = "Woo-hoo, Registrazione avvenuta!";
-        //                ScriptManager.RegisterStartupScript(this.Page, this.GetType(), "MostraModale", "$('#RegistrationModal').modal('show');", true);
-        //                string messaggio = "Benvenuto!";
-        //                ScriptManager.RegisterStartupScript(this.Page, this.GetType(), "alert", "alert('" + messaggio + "');", true);
-
-
-        //            }
-        //            catch (Exception ex)
-        //            {
-        //                // Log dell'errore e messaggio all'utente
-        //                Response.Write("Errore durante la registrazione.");
-        //                Response.Write(ex.Message);
-
-        //                // RegistrationModalBody.InnerText = "Ci dispiace ma il le password devono essere uguali!";
-        //                ScriptManager.RegisterStartupScript(this.Page, this.GetType(), "MostraModale", "$('#RegistrationModal').modal('show');", true);
-        //                string messaggio = "NO!";
-        //                ScriptManager.RegisterStartupScript(this.Page, this.GetType(), "alert", "alert('" + messaggio + "');", true);
-
-        //            }
-        //            finally
-        //            {
-        //                conn.Close();
-
-        //            }
-
-        //        }
-
-        //        else
-        //        {
-        //            //RegistrationModalBody.InnerText = "Arg, Registrazione fallita!";
-        //            ScriptManager.RegisterStartupScript(this.Page, this.GetType(), "MostraModale", "$('#RegistrationModal').modal('show');", true);
-        //            string messaggio = "No2!";
-        //            ScriptManager.RegisterStartupScript(this.Page, this.GetType(), "alert", "alert('" + messaggio + "');", true);
-
-        //        }
-
-
-
-
-        //    }
-
-
-
-        //}
-
-
-
-        //protected bool CheckUsername(string user)
-        //{
-
-        //    bool validUsername = true;
-
-        //    string connectionString = ConfigurationManager.ConnectionStrings["connectionStringDB"].ConnectionString.ToString();
-
-        //    try
-        //    {
-        //        using (SqlConnection conn = new SqlConnection(connectionString))
-        //        {
-        //            conn.Open();
-        //            SqlCommand cmd = new SqlCommand();
-        //            cmd.Connection = conn;
-        //            cmd.CommandText = "SELECT * FROM Utenti WHERE Username = @username";
-        //            cmd.Parameters.AddWithValue("@username", user);
-
-
-        //            using (SqlDataReader reader = cmd.ExecuteReader())
-        //            {
-        //                if (reader.Read())
-        //                {
-
-        //                    validUsername = false;
-        //                    conn.Close();
-        //                }
-        //            }
-        //        }
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        // Log dell'errore e messaggio all'utente
-        //        Response.Write("Errore durante la ricerca del nome.");
-        //        Response.Write(ex.Message);
-
-        //    }
-
-        //    return validUsername;
-
-        //}
     }
+
 }
